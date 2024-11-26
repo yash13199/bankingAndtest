1c470ad41d27985a50feddd9836fd8c1b6a2e1a6
yash13199 <52669742+yash13199@users.noreply.github.com>
2024-11-26 13:57:06 -0800

    add test cases

--- bankingAndtestTests/bankingAndtestTests.swift
+++ bankingAndtestTests/bankingAndtestTests.swift
@@ -8,29 +8,52 @@
 import XCTest
 @testable import bankingAndtest
 
-final class bankingAndtestTests: XCTestCase {
+//struct Errors {
+//    
+//     var invalidAmount : String
+//    invalidAmount = "Withdrawal amount must be positive"
+//    
+////    func invalidAount() -> Never {
+////        return "Withdrawal amount must be positive"
+////    }
+//}
 
+final class bankingAndtestTests: XCTestCase {
+    var account: Account?
+    //var errors: Errors?
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        account = Account(initialBalance: 100)
+        //errors = Errors()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        account = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func test_desposite(){
+        account?.deposit(amount: 100)
+        let result = account?.getBalance()
+        XCTAssertEqual(result, 200)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_withdraw(){
+        account?.withdraw(amount: 100)
+        let result = account?.getBalance()
+        XCTAssertEqual(result, 0)
     }
-
+    
+//    func test_withdraw_error(){
+//        let result = account?.withdraw(amount: 0) as Any
+//        
+//        XCTAssertEqual(
+//            result as! Never ,
+//            fatalError("Withdrawal amount must be positive")
+//        )
+////        
+//    }
+    
+    
+    
 }
