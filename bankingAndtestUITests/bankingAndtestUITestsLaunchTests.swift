fca4d186cda3752ac96ec1dce4ea39ce87e1cc9b
yash13199 <52669742+yash13199@users.noreply.github.com>
2024-11-26 10:45:04 -0800

    Initial Commit

---
+++ bankingAndtestUITests/bankingAndtestUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  bankingAndtestUITestsLaunchTests.swift
+//  bankingAndtestUITests
+//
+//  Created by Sohagia Yash on 11/26/24.
+//
+
+import XCTest
+
+final class bankingAndtestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
